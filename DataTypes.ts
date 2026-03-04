e809313b8c9edc58c11b7716a00dc93e0207b90e
Mohammad Ajmir <ajmerfaqiri14.af@gmail.com>
2026-03-04 13:12:24 +0430

    Create DataTypes.ts

---
+++ DataTypes.ts
@@ -0,0 +1,21 @@
+const isProgrammingFun: boolean = true;
+
+const total: number = 42;
+
+const name: string = "Ajmir";
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+const person: { name: string; age: number } = {
+  name: "Ajmir",
+  age: 25,
+};
+
+const cities: string[] = ["New York", "London", "Tokyo"];
+
+const classes: Array<string> = ["Math", "Science", "History"];
+
+const tuple: [string, number] = ["Ajmir", 25];
+
+let randomValue: any = 10;
+randomValue = "Now I'm a string";
